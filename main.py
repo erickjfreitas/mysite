8832f79f8e0e843ee5e1e4e900d4bdbb055f656a
LuisP30 <pimenta.lh2@gmail.com>
2023-05-26 19:34:32 -0300

    change main.py

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ def homepage():
 def contatos():
     return render_template("contatos.html")
 
-@app.route("/usuarios/<nome_usuario>")
-def usuarios(nome_usuario):
-    return render_template("usuarios.html", nome_usuario=nome_usuario)
+@app.route("/sobre")
+def usuarios():
+    return render_template("sobre.html")
 
 
 if __name__ == "__main__":
